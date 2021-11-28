74c7dd4545d98a1a1b31308763d9b94b6ede4d31
zlw-qa <asdori95@gmail.com>
2021-11-28 21:00:13 +0400

    add owner and CredentialsConfig.java

--- src/test/java/com/github/zlwqa/tests/TestBase.java
+++ src/test/java/com/github/zlwqa/tests/TestBase.java
@@ -4,21 +4,29 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.zlwqa.helpers.Attach;
 import com.github.zlwqa.pages.RegistrationsPage;
+import config.CredentialsConfig;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import static java.lang.String.format;
+
 public class TestBase {
 
     RegistrationsPage registrationsPage = new RegistrationsPage();
+    private static final CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
+    private static final String login = credentials.login();
+    private static final String password = credentials.password();
+    private static final String URL = credentials.URL();
+    private static final String remoteURL = format("https://%s:%s@%s", login, password, URL);
 
     @BeforeAll
     static void setup() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
-
+        Configuration.remote = remoteURL;
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
